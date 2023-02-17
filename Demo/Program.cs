d9a4fb99fa635ecd00c331566b0fd39969b08867
Callum Anning <Callum5042@gmail.com>
2023-02-17 10:48:19 +0000

    Add project files.

---
+++ Demo/Program.cs
@@ -0,0 +1,210 @@
+﻿using System;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.Runtime.InteropServices;
+using System.Threading;
+using System.Threading.Tasks;
+using SharpAvi;
+using SharpAvi.Codecs;
+using SharpAvi.Output;
+using System.Windows.Forms;
+
+namespace Demo
+{
+    // Used to Configure the Recorder
+    public class RecorderParams
+    {
+        public RecorderParams(string filename, int FrameRate, FourCC Encoder, int Quality)
+        {
+            FileName = filename;
+            FramesPerSecond = FrameRate;
+            Codec = Encoder;
+            this.Quality = Quality;
+
+            Height = Screen.PrimaryScreen.Bounds.Height;
+            Width = Screen.PrimaryScreen.Bounds.Width;
+        }
+
+        string FileName;
+        public int FramesPerSecond, Quality;
+        FourCC Codec;
+
+        public int Height { get; private set; }
+        public int Width { get; private set; }
+
+        public AviWriter CreateAviWriter()
+        {
+            return new AviWriter(FileName)
+            {
+                FramesPerSecond = FramesPerSecond,
+                EmitIndex1 = true,
+            };
+        }
+
+        public IAviVideoStream CreateVideoStream(AviWriter writer)
+        {
+            // Select encoder type based on FOURCC of codec
+            if (Codec == CodecIds.Uncompressed)
+            {
+                return writer.AddUncompressedVideoStream(Width, Height);
+            }
+            else if (Codec == CodecIds.MotionJpeg)
+            {
+                return writer.AddMJpegWpfVideoStream(Width, Height, Quality);
+            }
+            else
+            {
+                return writer.AddMpeg4VcmVideoStream(Width, Height, (double)writer.FramesPerSecond,
+                    // It seems that all tested MPEG-4 VfW codecs ignore the quality affecting parameters passed through VfW API
+                    // They only respect the settings from their own configuration dialogs, and Mpeg4VideoEncoder currently has no support for this
+                    quality: Quality,
+                    codec: Codec,
+                    // Most of VfW codecs expect single-threaded use, so we wrap this encoder to special wrapper
+                    // Thus all calls to the encoder (including its instantiation) will be invoked on a single thread although encoding (and writing) is performed asynchronously
+                    forceSingleThreadedAccess: true);
+            }
+        }
+    }
+
+    public class Recorder : IDisposable
+    {
+        #region Fields
+        AviWriter writer;
+        RecorderParams Params;
+        IAviVideoStream videoStream;
+        Thread screenThread;
+        ManualResetEvent stopThread = new ManualResetEvent(false);
+        #endregion
+
+        public Recorder(RecorderParams Params)
+        {
+            this.Params = Params;
+
+            // Create AVI writer and specify FPS
+            writer = Params.CreateAviWriter();
+
+            // Create video stream
+            videoStream = Params.CreateVideoStream(writer);
+            // Set only name. Other properties were when creating stream, 
+            // either explicitly by arguments or implicitly by the encoder used
+            videoStream.Name = "Captura";
+
+            screenThread = new Thread(RecordScreen)
+            {
+                Name = typeof(Recorder).Name + ".RecordScreen",
+                IsBackground = true
+            };
+
+            screenThread.Start();
+        }
+
+        public void Dispose()
+        {
+            stopThread.Set();
+            screenThread.Join();
+
+            // Close writer: the remaining data is written to a file and file is closed
+            writer.Close();
+
+            stopThread.Dispose();
+        }
+
+        void RecordScreen()
+        {
+            var frameInterval = TimeSpan.FromSeconds(1 / (double)writer.FramesPerSecond);
+            var buffer = new byte[Params.Width * Params.Height * 4];
+            Task videoWriteTask = null;
+            var timeTillNextFrame = TimeSpan.Zero;
+
+            while (!stopThread.WaitOne(timeTillNextFrame))
+            {
+                var timestamp = DateTime.Now;
+
+                Screenshot(buffer);
+
+                // Wait for the previous frame is written
+                videoWriteTask?.Wait();
+
+                // Start asynchronous (encoding and) writing of the new frame
+                videoWriteTask = videoStream.WriteFrameAsync(true, buffer, 0, buffer.Length);
+
+                timeTillNextFrame = timestamp + frameInterval - DateTime.Now;
+                if (timeTillNextFrame < TimeSpan.Zero)
+                    timeTillNextFrame = TimeSpan.Zero;
+            }
+
+            // Wait for the last frame is written
+            videoWriteTask?.Wait();
+        }
+
+        public void Screenshot(byte[] Buffer)
+        {
+            using (var BMP = new Bitmap(Params.Width, Params.Height))
+            {
+                using (var g = Graphics.FromImage(BMP))
+                {
+                    g.CopyFromScreen(Point.Empty, Point.Empty, new Size(Params.Width, Params.Height), CopyPixelOperation.SourceCopy);
+
+                    g.Flush();
+
+                    var bits = BMP.LockBits(new Rectangle(0, 0, Params.Width, Params.Height), ImageLockMode.ReadOnly, PixelFormat.Format32bppRgb);
+                    Marshal.Copy(bits.Scan0, Buffer, 0, Buffer.Length);
+                    BMP.UnlockBits(bits);
+                }
+            }
+        }
+    }
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //var writer = new AviWriter("test.avi")
+            //{
+            //    FramesPerSecond = 30,
+            //    // Emitting the AVI v1 index in addition to the OpenDML index (AVI v2)
+            //    // improves compatibility with some software, including 
+            //    // standard Windows programs like Media Player and File Explorer
+            //    EmitIndex1 = true
+            //};
+
+            //var stream = writer.AddVideoStream();
+
+            //// set standard VGA resolution
+            //stream.Width = 640;
+            //stream.Height = 480;
+            //// class SharpAvi.CodecIds contains FOURCCs for several well-known codecs
+            //// Uncompressed is the default value, just set it for clarity
+            //stream.Codec = CodecIds.MotionJpeg;
+            //// Uncompressed format requires to also specify bits per pixel
+            //stream.BitsPerPixel = BitsPerPixel.Bpp32;
+
+            //var frameData = new byte[stream.Width * stream.Height * 4];
+            //for (int i = 0; i < 1000; ++i)
+            //{
+            //    // fill frameData with image
+
+            //    // write data to a frame
+            //    stream.WriteFrame(true, // is a key frame? (many codecs use the concept of key frames, for others - all frames are keys)
+            //          frameData, // an array with frame data
+            //          0, // a starting index in the array
+            //          frameData.Length // a length of the data
+            //    );
+            //}
+
+            //stream.WriteFrame(true, frameData.AsSpan());
+            //writer.Close();
+
+
+            // Using MotionJpeg as Avi encoder,
+            // output to 'out.avi' at 10 Frames per second, 70% quality
+            var rec = new Recorder(new RecorderParams("out.avi", 10, CodecIds.MotionJpeg, 70));
+
+            Console.WriteLine("Press any key to Stop...");
+            Console.ReadKey();
+
+            // Finish Writing
+            rec.Dispose();
+        }
+    }
+}
